b75630030e77a8b6ce0350969df389f43e873835
joaosavio <joaosavio@gmail.com>
2010-04-25 16:06:59 -0300

    mudanca

--- ClientRest/src/clientrest/Main.java
+++ ClientRest/src/clientrest/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static final String BASE_URI = "http://localhost:8080/Rest";
    
     private static String addUser = "/resources/user/addUser";
-    private static String putUser = "/resources/user/updateUser/%s";
+    private static String putUser = "/resources/user/updateUser";
     private static String getUser = "/resources/user/getUser/%s";
     private static String deleteUser = "/resources/user/deleteUser/%s";
 
@@ -29,8 +29,14 @@ public class Main {
         User user = new User();
         user.setLogin("teste");
         user.setPassword("teste");
-        String param = String.format(addUser, user);
+        String param = addUser;
         time(post(param, user));
+
+        User user2 = new User();
+        user2.setLogin("teste");
+        user2.setPassword("teste3");
+        param = putUser;
+        time(put(param, user2));
 
         param = String.format(getUser, "teste");
         time(get(param));
@@ -57,13 +63,15 @@ public class Main {
         return result;
     }
 
-    public static long put(String path) {
+    public static long put(String path, User user) {
+        String json = new Gson().toJson(user, User.class);
         Client client = Client.create();
         WebResource wr = client.resource(BASE_URI);
         long millis = System.currentTimeMillis();
-        ClientResponse response = wr.path(path).put(ClientResponse.class);
+
+        ClientResponse response = wr.path(path).type(MediaType.APPLICATION_JSON).put(ClientResponse.class, json);
         long result = System.currentTimeMillis() - millis;
-    //    System.out.println(response.getEntity(String.class));
+        System.out.println(response);
 
         return result;
     }
@@ -85,7 +93,7 @@ public class Main {
         long millis = System.currentTimeMillis();
         String teste = wr.path(path).type(MediaType.APPLICATION_JSON).get(String.class);
         long result = System.currentTimeMillis() - millis;
-    //    System.out.println(teste);
+        System.out.println(teste);
 
         return result;
     }
