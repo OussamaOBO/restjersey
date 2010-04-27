1e4a27aca270e818fa2d567bde9a0e8d81a5a2ab
joaosavio <joaosavio@gmail.com>
2010-04-26 21:13:03 -0300

    mudanca

--- Rest/src/java/model/dao/UserDAO.java
+++ Rest/src/java/model/dao/UserDAO.java
@@ -1,7 +1,5 @@
 package model.dao;
 
-import com.sun.jersey.api.NotFoundException;
-import javax.ws.rs.WebApplicationException;
 import model.vo.User;
 import org.hibernate.Query;
 
@@ -15,23 +13,21 @@ public class UserDAO extends GenericDAO<User> {
         super(User.class);
     }
 
-    public User getUser(String login, String password) {
+    public User getUser(String login, String password) throws Exception {
         try {
             String hql = "select u from User as u where u.login = :login and u.password = :password";
             Query q = getSession().createQuery(hql);
             q.setParameter("login", login);
             q.setParameter("password", password);
-            User user = new User();
-            user = (User) q.uniqueResult();            
-            return user;
+            return (User) q.uniqueResult();  
         } catch (Exception e) {
-            throw new WebApplicationException(e, 500);
+            throw e;
         } finally {
             close();
         }
     }
 
-    public User getUser(String login) {
+    public User getUser(String login) throws Exception {
         return getById(login);
     }    
 }
